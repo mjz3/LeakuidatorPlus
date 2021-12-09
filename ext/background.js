544e36942f92cab529c30593f22e11a16ba3b001
m0jt4b4 <3613058+m0jt4b4@users.noreply.github.com>
2021-12-09 14:51:11 -0500

    delete performance entries when second response arrives

--- ext/background.js
+++ ext/background.js
@@ -780,7 +780,7 @@ function xhRequest(rId) {
 
         // trigger when second response headers received
         if(this.readyState == this.HEADERS_RECEIVED) {
-
+            performance.clearResourceTimings();
             // check for observable differences between two responses
             if(!headersEqual(responseOneData, getHeaderMap(request.getAllResponseHeaders()))) {
 
