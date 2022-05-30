e18446b8321d4d85e6a6cdf8c1d929d95a3d2f02
m0jt4b4 <3613058+m0jt4b4@users.noreply.github.com>
2022-05-30 19:45:56 -0400

    Commenting out Sec-Fetch-Site Browser implementaion is sometimes inaccurate. For example, when a request is made from https://www.yahoo.com to another host in yahoo.com, it is identified as crossSite. As such, conditions relying on the Sec-Fetch-Site metadata header are commented. I implemented an altertative function.

--- ext/background.js
+++ ext/background.js
@@ -350,12 +350,12 @@ function onBeforeSendHeaders(details) {
         if(extensionMode == "Relaxed") {
             // check Relaxed conditions on the request
             if(!isEmpty(sourceSite) && !isEmpty(targetSite)) {
-                modeConditions = ((!isEmpty(fetchSite) && fetchSite == "cross-site") || crossSite(sourceSite, targetSite) ? true : false);
+                modeConditions = (/*(!isEmpty(fetchSite) && fetchSite == "cross-site") ||*/ crossSite(sourceSite, targetSite) ? true : false);
             }
         } else if(extensionMode == "Exact") {
             // check Exact conditions on the request
             if (!isEmpty(sourceOrigin) && !isEmpty(targetOrigin)) {
-                modeConditions = ((!isEmpty(fetchSite) && fetchSite != "same-origin" && fetchSite != "none") || crossOrigin(sourceOrigin, targetOrigin)? true : false);
+                modeConditions = (/*(!isEmpty(fetchSite) && fetchSite != "same-origin" && fetchSite != "none") ||*/ crossOrigin(sourceOrigin, targetOrigin)? true : false);
             }
         }
     }
@@ -389,12 +389,12 @@ function onBeforeSendHeaders(details) {
             if(extensionMode == "Relaxed") {
                 // check Relaxed conditions on the request
                 if(!isEmpty(sourceSite) && !isEmpty(targetSite)) {
-                    modeConditions = ((!isEmpty(fetchSite) && fetchSite == "cross-site") || crossSite(sourceSite, targetSite) ? true : false);
+                    modeConditions = (/*(!isEmpty(fetchSite) && fetchSite == "cross-site") ||*/ crossSite(sourceSite, targetSite) ? true : false);
                 }
             } else if(extensionMode == "Exact") {
                 // check Exact conditions on the request
                 if (!isEmpty(sourceOrigin) && !isEmpty(targetOrigin)) {
-                    modeConditions = ((!isEmpty(fetchSite) && fetchSite != "same-origin" && fetchSite != "none") || crossOrigin(sourceOrigin, targetOrigin)? true : false);
+                    modeConditions = (/*(!isEmpty(fetchSite) && fetchSite != "same-origin" && fetchSite != "none") ||*/ crossOrigin(sourceOrigin, targetOrigin)? true : false);
                 }
             }
 
